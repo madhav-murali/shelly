9942f6fc21b3a8d985a4f53522224a3d93bc0f66
Madhav Murali <mdvmrli@gmail.com>
2026-05-01 07:40:12 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf("%s: command not found", line)
+	}
 }
