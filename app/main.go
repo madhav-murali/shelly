41dde560434a6322acd4aa8961f4142bae9b8a4d
Madhav Murali <mdvmrli@gmail.com>
2026-05-01 10:45:20 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
+	fmt.Print("$ ")
 	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error reading input:", err)
 		return
 	}
-	fmt.Print("$ " + cmd[:len(cmd)-1] + ": command not found")
+	fmt.Print(cmd[:len(cmd)-1] + ": command not found")
 	main()
 }
