53b3be412c7c7b8a499dcf68146661771a56c959
Madhav Murali <mdvmrli@gmail.com>
2026-05-13 11:32:55 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,10 +12,17 @@ import (
 	parser "github.com/codecrafters-io/shell-starter-go/internal/parser"
 )
 
-func runCmd(name string, args ...string) {
+func runCmd(name string, args ...string) []byte {
+	var stdoutBuf, stderrBuf bytes.Buffer
 	cmd := exec.Command(name, args...)
-	out, _ := cmd.CombinedOutput()
-	fmt.Print(string(out))
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+	_ = cmd.Run()
+	if stderrBuf.Len() > 0 {
+		fmt.Print(stderrBuf.String())
+	}
+	//out, _ := cmd.CombinedOutput()
+	return stdoutBuf.Bytes()
 }
 
 func main() {
@@ -36,21 +44,41 @@ func main() {
 	validCmds.Add("PWD")
 	validCmds.Add("CD")
 
-	//TODO: add ' quote capability.
+	var redirects bool
+	var fileName string
+	// if strings.Contains(line, ">") {
+	// 	redirects = true
+	// 	idx := strings.Index(line, ">")
+	// 	fileName = strings.TrimSpace(line[idx+1:])
+	// 	if idx > 0 && line[idx-1] != '1' {
+	// 		line = line[:idx]
+	// 	} else {
+	// 		line = line[:idx-1]
+	// 	}
+	// }
 
 	args := parser.ParseLine(line)
+
+	for i, arg := range args {
+		if arg == ">" || arg == "1>" {
+			redirects = true
+			fileName = args[i+1]
+			args = args[:i]
+			break
+		}
+	}
 	cmd := args[0]
 	switch strings.ToUpper(cmd) {
 	case "EXIT":
 		os.Exit(0)
-	case "ECHO":
-		for i := 1; i < len(args); i++ {
-			fmt.Print(args[i])
-			if i != len(args)-1 {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Print("\n")
+	// case "ECHO":
+	// 	for i := 1; i < len(args); i++ {
+	// 		fmt.Print(args[i])
+	// 		if i != len(args)-1 {
+	// 			fmt.Print(" ")
+	// 		}
+	// 	}
+	// 	fmt.Print("\n")
 	case "TYPE":
 		if validCmds.Find(strings.ToUpper(args[1])) {
 			fmt.Printf("%s is a shell builtin\n", args[1])
@@ -90,7 +118,30 @@ func main() {
 			fmt.Printf("%s: not found\n", args[0])
 			break
 		}
-		runCmd(args[0], args[1:]...)
+
+		// if slices.Contains(args, ">") {
+		// 	redirects = true
+		// 	idx := slices.Index(args, ">")
+		// 	fileName = strings.Join(args[idx+1:], "")
+		// 	args = args[:idx]
+		// }
+		output := runCmd(args[0], args[1:]...)
+		if !redirects {
+			fmt.Print(string(output))
+		} else {
+			// dir := filepath.Dir(fileName)
+			// err = os.Mkdir(dir, 0755)
+			// if err != nil {
+			// 	panic(err)
+			// }
+			file, err := os.OpenFile(fileName, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
+
+			file.Write(output)
+		}
 	}
 	main()
 }
